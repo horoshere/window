0276df36dfaf007d7de957f0cea6c7142cada4d4
Nikolay <niktyler7@gmail.com>
2022-10-02 18:18:22 +0300

    Modal window functionality.

---
+++ src/js/modules/modals.js
@@ -0,0 +1,49 @@
+const modals = () => {
+    
+    function bindModal(triggerSelector, modalSelector, closeSelector) {
+        const trigger = document.querySelectorAll(triggerSelector),
+        modal = document.querySelector(modalSelector),
+        close = document.querySelector(closeSelector);
+
+        trigger.forEach(item => {
+            item.addEventListener('click', (e) => {
+                if (e.target) {
+                    e.preventDefault();
+                }
+                
+                modal.style.display = 'block';
+                //scroll block
+                document.body.style.overflow = 'hidden';
+                //вместо overflow: hidden используем класс из bootstrap
+                // document.body.classList.add('modal-open');
+            });
+        });
+
+        close.addEventListener('click', () => {
+            modal.style.display = 'none';
+            document.body.style.overflow = '';
+            // document.body.classList.remove('modal-open');
+        });
+
+        modal.addEventListener('click', (e) => {
+            if (e.target === modal) {
+                modal.style.display = 'none';
+                document.body.style.overflow = '';
+                // document.body.classList.remove('modal-open');
+            }
+        })
+    };
+
+    // function showModalByTime(selector, time) {
+    //     setTimeout(() => {
+    //         document.querySelector(selector).style.dispaly = 'block';
+    //         document.body.style.overflow = 'hidden';
+    //     }, time);
+    // }
+    
+    bindModal('.popup_engineer_btn', '.popup_engineer', '.popup_engineer .popup_close');
+    bindModal('.phone_link', '.popup', '.popup .popup_close');
+    // showModalByTime('.popup', 3000);
+};
+
+export default modals;
