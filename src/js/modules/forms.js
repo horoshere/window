0a2db18a5cf7b8030a9cefb550cc98cac23cfa24
Nikolay <niktyler7@gmail.com>
2022-10-25 20:36:30 +0300

    Working with forms. FormData and fetch response.

---
+++ src/js/modules/forms.js
@@ -0,0 +1,53 @@
+const forms = () => {
+    const form = document.querySelectorAll('form'),
+        input = document.querySelectorAll('input');
+
+    const message = {
+        loading: 'Загрузка...',
+        succes: 'Спасибо!',
+        failure: 'Что-то пошло не так...'
+    };
+
+    const postData = async (url, data) => {
+        document.querySelector('status').textContent = message.loading;
+        let res = await fetch(url, {
+            method: 'POST',
+            body: data
+        });
+
+        return await res.text()
+    };
+
+    const clearInput = () => {
+        input.forEach(item => {
+            item.value = '';
+        });
+    }
+
+    form.forEach(item => {
+        item.addEventListener('submit', (e) => {
+            e.preventDefault;
+
+            let statusMessage = document.createElement('div');
+            statusMessage.classList.add('status');
+            item.appendChild(statusMessage);
+
+            const formData = new FormData(item);
+
+            postData('assets/server.php', formData)
+                .then(res => {
+                    console.log(res);
+                    statusMessage.textContent = message.succes;
+                })
+                .catch(() => statusMessage.textContent = message.failure)
+                .finally(() => {
+                    clearInput();
+                    setTimeout(() => {
+                        statusMessage.remove();
+                    }, 5000);
+                });
+        });
+    });
+};
+
+export default forms;
